159ecb5996c77c605efd5970a5b9a2934de65151
Alexandr Kramar <akramar71@mail.ru>
2023-11-16 23:05:53 +0600

    Переделал обработчик клика в galleryObjectModule. Убрал второй обработчик клика из galleryWorkModule. Собрал экспортируемые имена классов для galleryObjectModule в один объект.

--- js/galleryWorkModule.js
+++ js/galleryWorkModule.js
@@ -1,4 +1,4 @@
-import { BIG_PICTURE_CLOSE_BUTTON_CLASS, BIG_PICTURE_CONTAINER_CLASS, PICTURE_IMAGE_CLASS } from './constants.js';
+import { BIG_PICTURE_CLOSE_BUTTON_CLASS, BIG_PICTURE_CONTAINER_CLASS/*, PICTURE_IMAGE_CLASS*/ } from './constants.js';
 import { getUserPhotos } from './data.js';
 import { fullSizePhoto } from './fullSizePhotoModule.js';
 import { thumbnailsGallery } from './galleryObjectModule.js';
@@ -6,11 +6,3 @@ import { thumbnailsGallery } from './galleryObjectModule.js';
 thumbnailsGallery.init(getUserPhotos(), 'picture', 'pictures');
 
 fullSizePhoto.init(BIG_PICTURE_CONTAINER_CLASS, BIG_PICTURE_CLOSE_BUTTON_CLASS);
-
-const onThumbnailsContainerClick = function (evt) {
-  if (evt.target.className === PICTURE_IMAGE_CLASS) {
-    fullSizePhoto.show(thumbnailsGallery.clickedPicture);
-  }
-};
-
-thumbnailsGallery.thumbnailsContainer.addEventListener('click', onThumbnailsContainerClick);
